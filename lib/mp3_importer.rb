44c047d7bb666bb354a65aa6594ecb9b128a385f
Jae Yong Kim <kim900180@gmail.com>
2018-08-09 19:38:47 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,7 +13,7 @@ class MP3Importer
     files
   end
 
-  def import(list_of_filenames)
-    list_of_filenames.each { |filename| Song.new_by_filename(filename) }
+  def import
+    self.files.each { |filename| Song.new_by_filename(filename) }
   end
 end
