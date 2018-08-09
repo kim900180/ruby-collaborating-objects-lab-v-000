6835a6380ea72e936e3f918431524d24e9d5983a
Jae Yong Kim <kim900180@gmail.com>
2018-08-09 19:34:53 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     if self.all.detect {|artist| artist.name == name}
-
+      artist.name = name
     else
       artist = Artist.new(name)
       artist.save
