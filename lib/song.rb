849ca442f9b65d1a623fec0750088811dbb9e8e9
Jae Yong Kim <kim900180@gmail.com>
2018-08-09 19:39:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song_file = file.chomp(".mp3").split(" - ")
+    song_file = filename.chomp(".mp3").split(" - ")
     song = Song.new(song_file[1])
     song.artist_name = song_file[0]
     song
